61ce214fe5cf07f0602fa4788c410f3294bd9bb3
teateecha <chagen@c1b5c1.42berlin.de>
2026-08-06 15:07:38 +0200

    FIX megaphone: use std:cout syntax

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,6 +1,5 @@
-#include <ctype.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <ctype.h>/*for toupper*/
+#include <iostream>
 
 int	main(int argc, char **argv)
 {
@@ -8,23 +7,24 @@ int	main(int argc, char **argv)
 	int		i;
 	int		j;
 
-	i = 0;
 	j = 1;
 	if (argc == 1)
 	{
-		write(1, "* LOUD AND UNBEARABLE FEEDBACK NOISE *", 38);
+		std::cout <<  "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	while (argv[j])
+	while (j < argc)
 	{
-		while(argv[1][i])
+		i = 0;
+		while(argv[j][i])
 		{
-			c = argv[1][i];
-			putchar(toupper(c));
+			c = toupper(argv[j][i]);
+			std::cout << c;
 			i++;
 		}
 		j++;
 	}
+	std::cout << std::endl;
 	return (0);
 }
 
