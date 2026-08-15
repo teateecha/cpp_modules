96348cc06044dd1bea6e5dce17b44d009fb560a1
teateecha <chagen@c4b12c2.42berlin.de>
2026-08-15 19:30:11 +0200

    ADD extra files for functions

--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -14,4 +14,7 @@ class Zombie
 		std::string _name;
 };
 
+Zombie*	newZombie(std::string name);
+void randomChump(std::string name);
+
 #endif
---
+++ cpp01/ex00/randomChump.cpp
@@ -0,0 +1,9 @@
+#include "Zombie.hpp"
+
+void randomChump(std::string name)
+{
+	Zombie random(name);
+	
+	random.announce();
+	return ;
+}
