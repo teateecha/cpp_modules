272bd477098e527f17d0fe6a1bb91aacd6b980cc
user@clarasPC <user@clarasPC.de>
2026-09-07 20:03:54 +0200

    ADD cpp02/ex00

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,19 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+	public:
+		Fixed();//Default Constructor
+		Fixed(const Fixed& other);// Copy Constructor
+		Fixed& 				operator=(const Fixed& other);//Copy assignement operator
+		~Fixed();//Destructor
+
+		int					getRawBits(void) const;
+		void				setRawBits(int const raw);
+	private:
+		int					_value;
+		static const int	_fractional = 8;
+};
+
+#endif
