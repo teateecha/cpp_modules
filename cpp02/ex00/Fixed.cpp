72781da6c453b5f2b29168b5b0760e8afb67040c
user@clarasPC <user@clarasPC.de>
2026-09-08 12:28:17 +0200

    EDIT ex00 added explaining comments

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,18 +6,20 @@ Fixed::Fixed() : _value(0)
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& other)
+//construct and copy a new object from an existing (other)
+Fixed::Fixed(const Fixed& other)// : _value(other._value) this woud be cleaner
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
+//copy into an existing object from an other existing object
 Fixed& Fixed::operator=(const Fixed& other)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 	{
-		_value = other.getRawBits();
+		_value = other.getRawBits();//only for the subjet output other._value would be simpler
 	}
 	return *this;
 }
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -13,7 +13,7 @@ class Fixed
 		void				setRawBits(int const raw);
 	private:
 		int					_value;
-		static const int	_fractional = 8;
+		static const int	_fractionalBits = 8;//not per object e e
 };
 
 #endif
