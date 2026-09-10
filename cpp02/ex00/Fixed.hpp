0b76a1905a1b7e25203eca743bd8373df2dac233
teateecha <chagen@c4b2c1.42berlin.de>
2026-09-10 17:58:14 +0200

    TYPO

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -13,7 +13,7 @@ class Fixed
 		void				setRawBits(int const raw);
 	private:
 		int					_value;
-		static const int	_fractionalBits = 8;//not per object e e
+		static const int	_fractionalBits = 8;//not per object
 };
 
 #endif
