87b727a304e0ba8daf8c7ab46cbd739de6e63753
teateecha <chagen@c4b2c1.42berlin.de>
2026-09-10 17:01:38 +0200

    EDIT cpp1ex4 add check for empty string

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -55,9 +55,11 @@ int	main(int argc, char **argv)
 	if (!inputfile.is_open())
 		return (complainAndReturn("input file not readable", 1));
 
+	if (std::string(argv[2]).empty())
+		return (complainAndReturn("empty strings"), 1)
 	std::string	outputName = std::string(argv[1]) + ".replace";
 	std::ofstream	output(outputName.c_str());
-	if (!output.is_open() || output.empty())
+	if (!output.is_open())
 		return (complainAndReturn("output file not created", 1));
 
 	retu = ft_replace (inputfile, output, argv[2], argv[3]);
