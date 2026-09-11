dec24ceaabf92a7d7e6f1aac59497df0a0a87db0
teateecha <chagen@c3a1c7.42berlin.de>
2026-09-11 12:45:44 +0200

    ADD tests for operations css02ex02

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -16,3 +16,37 @@ int main( void )
 
 	return (0);
 }
+
+// int main(void)
+// {
+// 	Fixed a(5.5f);
+// 	Fixed b(2);
+//
+// 	std::cout << "a = " << a << ", b = " << b << std::endl;
+//
+// 	std::cout << "a + b = " << a + b << std::endl;
+// 	std::cout << "a - b = " << a - b << std::endl;
+// 	std::cout << "a * b = " << a * b << std::endl;
+// 	std::cout << "a / b = " << a / b << std::endl;
+//
+// 	std::cout << "a > b  : " << (a > b) << std::endl;
+// 	std::cout << "a < b  : " << (a < b) << std::endl;
+// 	std::cout << "a >= b : " << (a >= b) << std::endl;
+// 	std::cout << "a <= b : " << (a <= b) << std::endl;
+// 	std::cout << "a == b : " << (a == b) << std::endl;
+// 	std::cout << "a != b : " << (a != b) << std::endl;
+//
+// 	Fixed c;
+// 	std::cout << "c     = " << c << std::endl;
+// 	std::cout << "++c   = " << ++c << std::endl;
+// 	std::cout << "c++   = " << c++ << std::endl;
+// 	std::cout << "c     = " << c << std::endl;
+// 	std::cout << "--c   = " << --c << std::endl;
+// 	std::cout << "c--   = " << c-- << std::endl;
+// 	std::cout << "c     = " << c << std::endl;
+//
+// 	std::cout << "min(a,b) = " << Fixed::min(a, b) << std::endl;
+// 	std::cout << "max(a,b) = " << Fixed::max(a, b) << std::endl;
+//
+// 	return (0);
+// }
