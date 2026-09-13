87daa106cb4491b1d4303ba4a759ea165bd5e443
teateecha <218916862+teateecha@users.noreply.github.com>
2026-09-13 16:50:30 +0200

    EDIT claptrap ex00 fixing engery points error

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -1,6 +1,7 @@
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 
+#include <string>//for std
 class ClapTrap
 {
 	public:
@@ -12,7 +13,7 @@ class ClapTrap
 
 		void			attack(const std::string& target);
 		void			takeDamage(unsigned int amount);
-		void			beRepaired(unsigned int amound);
+		void			beRepaired(unsigned int amount);
 
 	private:
 		std::string		_name;
