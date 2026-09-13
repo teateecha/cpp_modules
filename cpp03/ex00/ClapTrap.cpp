58f8830e0e3df52f234edc63eaadca7a595c9b47
teateecha <218916862+teateecha@users.noreply.github.com>
2026-09-13 17:04:26 +0200

    TYPO nicer messages now

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -11,14 +11,14 @@ ClapTrap::ClapTrap()
 ClapTrap::ClapTrap(std::string name)
 	: _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
 {
-	std::cout << "ClapTrap: " << _name << "Constructor called" << std::endl;
+	std::cout << "ClapTrap " << _name << ": Constructor called" << std::endl;
 }
 
 // Copy constructor
 ClapTrap::ClapTrap(const ClapTrap& other)
 {
 	*this = other;
-	std::cout << "ClapTrap: " << _name << "copy-constructed." << std::endl;
+	std::cout << "ClapTrap " << _name << ": copy-constructed." << std::endl;
 }
 
 // Copy assignment operator
@@ -31,14 +31,13 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& other)
 		_energyPoints = other._energyPoints;
 		_attackDamage = other._attackDamage;
 	}
-	std::cout << "ClapTrap: " << _name << "assigned." << std::endl;
 	return *this;
 }
 
 // Destructor
 ClapTrap::~ClapTrap()
 {
-	std::cout << "ClapTrap: " << _name << "Destructor called" << std::endl;
+	std::cout << "ClapTrap " << _name << ": Destructor called" << std::endl;
 }
 
 void	ClapTrap::attack(const std::string& target)
@@ -62,7 +61,7 @@ void	ClapTrap::attack(const std::string& target)
 			<< _attackDamage
 			<< " points of damage!"
 			<< " Energy points remaining: "
-			<< _energyPoints << std::endl;
+			<< _energyPoints << "." << std::endl;
 	}
 }
 
@@ -84,24 +83,24 @@ void	ClapTrap::takeDamage(unsigned int amount)
 void	ClapTrap::beRepaired(unsigned int amount)
 {
 	if (_energyPoints == 0)
-		std::cout << "ClapTrap"
+		std::cout << "ClapTrap "
 			<< _name
 			<< " cannot repair itself: no energy points left." << std::endl;
-	if (_hitPoints == 0)
-		std::cout << "ClapTrap"
+	else if (_hitPoints == 0)
+		std::cout << "ClapTrap "
 			<< _name
 			<< " cannot repair itself: no hit points left." << std::endl;
 	else
 	{
 		_hitPoints += amount;
-		_energyPoints-- ;
+		_energyPoints--;
 		std::cout << "ClapTrap "
 			<< _name
-			<< " repairs itself gaining "
+			<< " repairs itself, gaining "
 			<< amount
 			<< " hit points. Hit points: "
 			<< _hitPoints
-			<< " energy Points: "
+			<< ". Energy points: "
 			<< _energyPoints << std::endl;
 	}
 }
