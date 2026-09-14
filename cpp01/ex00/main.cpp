63f54dc90428dbfc750b41d9e2171abeef5225f0
teateecha <chagen@c4c2c3.42berlin.de>
2026-09-14 18:34:44 +0200

    cc01/ex00 forgotten functions deleted

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -1,18 +1,4 @@
 #include "Zombie.hpp"
-/*new keyword: allocate memory on heap and free it with delete*/
-Zombie*	newZombie(std::string name)
-{
-	return (new Zombie(name));
-}
-
-void randomChump(std::string name)
-{
-	Zombie random(name);
-	
-	random.announce();
-	return ;
-}
-
 
 int	main(void)
 {
