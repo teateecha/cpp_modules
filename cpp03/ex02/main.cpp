775a5d650fbf18cc4a4cca482993180db8a04264
teateecha <218916862+teateecha@users.noreply.github.com>
2026-09-16 19:38:26 +0200

    ADD ex02

---
+++ cpp03/ex02/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "ScavTrap.hpp" //has ClapTrap included
+
+int	main(void)
+{
+	const std::string	victim = "Victim Tom";
+
+	std::cout << "\n--- DAMAGE / DEATH TEST ---\n";
+	ClapTrap felix("Felix");
+
+	felix.attack(victim);
+	felix.takeDamage(5);
+	felix.beRepaired(1);
+	felix.takeDamage(7);
+	felix.attack(victim); // no hit points left
+
+	std::cout << "\n--- ENERGY TEST ---\n";
+	ClapTrap anna("Anna");
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "Attack " << i + 1 << ": ";
+		anna.attack(victim);
+	}
+
+	anna.beRepaired(5); // no energy points left
+	anna.attack(victim); // no energy points left
+
+	std::cout << "\n ----ScavTrap TEST ---\n";
+	ScavTrap	sonya("Sonya");
+
+	sonya.attack(victim); //should show 20 damage
+	sonya.guardGate();
+
+	std::cout << "\n DESTRUCTORS: --- " << std::endl;
+	return (0);
+}
