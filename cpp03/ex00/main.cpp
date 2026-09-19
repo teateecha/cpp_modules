4a19f49acc72ba3e5b2a1186beea6469a43db1e2
teateecha <chagen@c3a6c1.42berlin.de>
2026-09-19 18:05:56 +0200

    EDIT cpp03ex00 tests

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -5,18 +5,22 @@ int	main(void)
 {
 	const std::string	victim = "Victim Tom";
 
+	{
 	std::cout << "\n--- DAMAGE / DEATH TEST ---\n";
 	ClapTrap felix("Felix");
-
 	felix.attack(victim);
 	felix.takeDamage(5);
+	ClapTrap copy_of_felix(felix);
 	felix.beRepaired(1);
 	felix.takeDamage(7);
 	felix.attack(victim); // no hit points left
+	copy_of_felix.beRepaired(5);// this will work
+	std::cout << "\n DESTRUCTOR: --- " << std::endl;
+	}
 
 	std::cout << "\n--- ENERGY TEST ---\n";
-	ClapTrap anna("Anna");
 
+	ClapTrap anna("Anna");
 	for (int i = 0; i < 10; i++)
 	{
 		std::cout << "Attack " << i + 1 << ": ";
@@ -26,7 +30,6 @@ int	main(void)
 	anna.beRepaired(5); // no energy points left
 	anna.attack(victim); // no energy points left
 
-	std::cout << "\n DESTRUCTORS: --- " << std::endl;
+	std::cout << "\n DESTRUCTOR: --- " << std::endl;
 	return (0);
-}
 }
