720564983eb367e5d03390f6f5652bc82055d035
teateecha <chagen@c1b4c2.42berlin.de>
2026-09-22 10:58:56 +0200

    ADD test for wrong animal in main.cpp cpp04ex00

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,18 +6,37 @@
 int main()
 {
 	std::cout << "\n--- TEST from Subject ---" << std::endl;
-	const Animal*	meta = new Animal();
-	const Animal*	j = new Dog();
-	const Animal*	i = new Cat();
+	{
+		const Animal*	meta = new Animal();
+		const Animal*	j = new Dog();
+		const Animal*	i = new Cat();
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
 
-	i->makeSound();
-	//will output the cat sound! j->makeSound()
-	meta->makeSound();
-	std::cout << "\n--- operator overload test ---" << std::endl;
-	std::cout << "\n--- sound test ---" << std::endl;
+		i->makeSound();//will output the cat sound!
+		j->makeSound();
+		meta->makeSound();
+		delete meta;
+		delete j;
+		delete i;
+	}
+	// std::cout << "\n--- Wrong Animal TEST ---" << std::endl;
+	// {
+	// 	const WrongAnimal*	meta = new WrongAnimal();
+	// 	const WrongAnimal*	j = new Dog();
+	// 	const WrongAnimal*	i = new Cat();
+	//
+	// 	std::cout << j->getType() << " " << std::endl;
+	// 	std::cout << i->getType() << " " << std::endl;
+	//
+	// 	i->makeSound();//will output the cat sound!
+	// 	j->makeSound();
+	// 	meta->makeSound();
+	// 	delete meta;
+	// 	delete j;
+	// 	delete i;
+	// }
 
 	return 0;
 }
